5ff506262be34fcbffa0e23b3b5f1a63266b79e9
José Manuel Cota <jmcota@ontica.org>
2024-04-22 18:01:14 -0400

    BudgetAccountSegment and BudgetAccountSegmentType power type added

---
+++ Core/Accounts/Domain/BudgetAccountSegmentType.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Accounts                            Component : Domain Layer                            *
+*  Assembly : Empiria.Budgeting.Core.dll                 Pattern   : Power type                              *
+*  Type     : BudgetAccountSegmentType                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Power type that describes a budget account segment.                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Ontology;
+
+namespace Empiria.Budgeting {
+
+  /// <summary>Power type that describes a budget account segment.</summary>
+  [Powertype(typeof(BudgetAccountSegment))]
+  public sealed class BudgetAccountSegmentType : Powertype {
+
+    #region Constructors and parsers
+
+    private BudgetAccountSegmentType() {
+      // Empiria powertype types always have this constructor.
+    }
+
+    static public new BudgetAccountSegmentType Parse(int typeId) {
+      return ObjectTypeInfo.Parse<BudgetAccountSegmentType>(typeId);
+    }
+
+    static public new BudgetAccountSegmentType Parse(string typeName) {
+      return BudgetAccountType.Parse<BudgetAccountSegmentType>(typeName);
+    }
+
+    #endregion Constructors and parsers
+
+  } // class BudgetAccountSegmentType
+
+} // namespace Empiria.Budgeting
