2492f20ecdbd61ad92c74f6e29cc208f90d73303
José Manuel Cota <jmcota@ontica.org>
2024-05-28 21:38:18 -0600

    Budgeting Processes module added

---
+++ Processes/Domain/BudgetProcessType.cs
@@ -0,0 +1,27 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Procesess                           Component : Domain Layer                            *
+*  Assembly : Empiria.Budgeting.Processes.dll            Pattern   : Infomation Holder                       *
+*  Type     : BudgetProcessType                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a budget process type.                                                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Workflow.Definition;
+
+namespace Empiria.Budgeting.Processes {
+
+  /// <summary>Represents a budget process type.</summary>
+  public class BudgetProcessType : ProcessType {
+
+    public FixedList<Budget> Budgets {
+      get {
+        return ExtendedDataField.GetFixedList<Budget>("budgets");
+      }
+    }
+
+  }  // class BudgetProcessType
+
+}  // namespace Empiria.Budgeting.Processes
