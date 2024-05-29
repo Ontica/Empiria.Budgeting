94adc05789938e6a32823896df0f47dc220362a7
José Manuel Cota <jmcota@ontica.org>
2024-05-28 21:38:38 -0600

    Budgeting Transactions module added

---
+++ Transactions/Domain/BudgetTransactionType.cs
@@ -0,0 +1,19 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Transactions                        Component : Domain Layer                            *
+*  Assembly : Empiria.Budgeting.Transactions.dll         Pattern   : Infomation Holder                       *
+*  Type     : BudgetTransactionType                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a budget transaction type.                                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Budgeting.Transactions {
+
+  /// <summary>Represents a budget transaction type.</summary>
+  public class BudgetTransactionType : GeneralObject {
+
+  }  // class BudgetTransactionType
+
+}  // namespace Empiria.Budgeting.Transactions
